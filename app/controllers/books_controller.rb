b14be2e0e1fe824ac383b1771f993a745565d6de
EC2 Default User <ec2-user@ip-172-31-47-90.ec2.internal>
2021-03-23 07:23:18 +0000

    [13]投稿詳細と投稿一覧に投稿ユーザの画像表示を実装、投稿者本人のみ編集と削除機能を表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,7 +16,6 @@ class BooksController < ApplicationController
   
   def show
     @book = Book.find(params[:id])
-    
   end
   
   def edit
@@ -32,6 +31,11 @@ class BooksController < ApplicationController
   
   
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+    # 投稿一覧へリダイレクト
+    
   end
   
   private
