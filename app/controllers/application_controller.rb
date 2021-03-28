4f8e99146340d6a9363c2144802365cecfe5df9d
EC2 Default User <ec2-user@ip-172-31-47-90.ec2.internal>
2021-03-28 14:30:04 +0000

    [17]レイアウト調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   
   def after_sign_in_path_for(resource)
-    books_path
+    user_path(current_user.id)
     # 新規登録後投稿一覧画面へ行くように変更する
     # ユーザ情報詳細へ行くよう変更する
   end
