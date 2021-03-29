58ea4300970dfdf235b5c84a23997e733912b4ac
EC2 Default User <ec2-user@ip-172-31-47-90.ec2.internal>
2021-03-29 07:14:32 +0000

    [18]入力フォームにフォームグループを適用

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a08221e65afc749d7616d3ee4b25acc0926af295cabf448d9858b6ef849f0937ccbaeaf88abddc8acfefa4a52142652d32f121662d320e7f46797ea002504f32'
